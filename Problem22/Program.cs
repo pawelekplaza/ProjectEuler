c6c3254fb643788d8732e3a13ea2058e41d4f01d
Paweł Płaza <pawelekplaza@gmail.com>
2018-08-14 14:57:28 +0200

    Added problem 22.

---
+++ Problem22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+
+namespace Problem22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var names = File.ReadAllText("names.txt").Replace("\"", "").Split(',');
+            Array.Sort(names);
+            Console.WriteLine($"Result: { GetScoreSum(names) }");
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time: { stopwatch.ElapsedMilliseconds } ms.");
+            Console.ReadLine();
+        }
+
+        private static BigInteger GetScoreSum(string[] names)
+        {
+            BigInteger score = new BigInteger(0);
+            for (int i = 0; i < names.Length; i++)
+                score += GetScore(names[i], i);
+            return score;
+        }
+
+        private static long GetScore(string name, int index)
+        {
+            long score = 0;
+            for (int i = 0; i < name.Length; i++)
+                score += name[i] - 'A' + 1;
+            return (index + 1) * score;
+        }
+    }
+}
